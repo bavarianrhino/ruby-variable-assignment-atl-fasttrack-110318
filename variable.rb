0076abad40a352d7efda58a9aaaee2e9a37464a7
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-11-04 18:43:45 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,17 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 
-greeting = "Hello World"
+# greeting = "Hello World"
+
+
+
+50.times (i)
+  times.
+  
+
+
+
+
+
+
+
