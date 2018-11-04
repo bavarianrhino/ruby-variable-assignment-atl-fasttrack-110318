a8dc8053ebf680aa6e3ed17f1461a58de896d46e
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-11-04 20:23:51 +0000

    automatically backed up by learn

--- badges.rb
+++ badges.rb
@@ -7,10 +7,10 @@ badge_maker("Ryan")
 
 greetings = []
 arr_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def batch_badge_creator (i)
-  arr_names.each do |i|
-  greetings << "Hello, my name is #{arr_names[i]}!"
-end
+def batch_badge_creator (arg)
+  arg.each do |i|
+    greetings << "Hello, my name is #{arg[i]}!"
+  end
 end
 
 batch-badge_creator(arr_names)
